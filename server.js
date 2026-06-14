deae9ef8c47ec57dba716474d0ed3edad9def1b0
vimalRaj45 <vimalr@spirelia.com>
2026-06-15 01:31:08 +0530

    fix: load standalone html2canvas and jsPDF CDNs to fix ReferenceError

--- server.js
+++ server.js
@@ -3052,8 +3052,9 @@ fastify.get('/certificate/:certNoEncoded', async (request, reply) => {
   
   <!-- qrcode.js CDN -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"></script>
-  <!-- html2pdf.js CDN for client-side rendering -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>
+  <!-- html2canvas and jsPDF CDNs for client-side rendering -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
 
   <style>
     /* Styling for Screen Preview */
