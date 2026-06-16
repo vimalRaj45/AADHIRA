9b15888fbae55271ce4c19ff6aa6d60ef47f97f0
vimalRaj45 <vimalr@spirelia.com>
2026-06-16 18:36:27 +0530

    Remove Share Feed button, keep only Add to LinkedIn Profile button

--- server.js
+++ server.js
@@ -4252,12 +4252,6 @@ fastify.get('/certificate/:certNoEncoded', async (request, reply) => {
       </svg>
       Download PDF
     </button>
-    <a class="btn btn-back" href="#" onclick="shareOnLinkedIn(event)" title="Share this certificate link to your LinkedIn Feed">
-      <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor" stroke="none">
-        <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
-      </svg>
-      Share Feed
-    </a>
     <a class="btn btn-back" href="#" onclick="addToLinkedInProfile(event)" title="Add this certificate to your LinkedIn Profile's Licenses & Certifications section">
       <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor" stroke="none">
         <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
@@ -4296,17 +4290,7 @@ fastify.get('/certificate/:certNoEncoded', async (request, reply) => {
 
     });
 
-    function shareOnLinkedIn(e) {
-      e.preventDefault();
-      let shareUrl = window.location.href;
-      const host = window.location.hostname;
-      // If we are testing locally, override with production URL so LinkedIn can scrape it
-      if (host === 'localhost' || host === '127.0.0.1' || host.startsWith('192.') || host.startsWith('10.') || host.startsWith('172.')) {
-        shareUrl = 'https://aadhira.onrender.com/certificate/${certNoEncoded}';
-      }
-      const url = encodeURIComponent(shareUrl);
-      window.open(\`https://www.linkedin.com/sharing/share-offsite/?url=\${url}\`, '_blank');
-    }
+
 
     function addToLinkedInProfile(e) {
       e.preventDefault();
